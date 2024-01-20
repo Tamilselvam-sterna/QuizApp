23fe28cd98293dc6f9c1ccf2acc3cfac9202788c
Ragupathi-Sterna <ragupathic@sternadevices.in>
2024-01-20 10:11:05 +0530

    removed oraganized import plugin

--- src/pages/admin/dashboard/Dashboard.tsx
+++ src/pages/admin/dashboard/Dashboard.tsx
@@ -1,27 +1,12 @@
-import { Variants, motion } from "framer-motion";
-import CardWrapper from "../../../components/Card";
 import { useState, useEffect } from "react";
 import { DashBoardStats } from "../../../models/dashboard";
 import { apiProvider } from "../../../network/apiProvider";
 import LatestResults from "./LatestResults";
 import BarChart from "./BarChart";
 
-const childrenVariant: Variants = {
-  initial: {
-    x: -100,
-  },
-  animate: {
-    x: 0,
-    transition: {
-      duration: 1,
-      staggerChildren: 0.3,
-    },
-  },
-};
-
 const Dashboard = () => {
   const [dashboardData, setDashboardData] = useState<DashBoardStats | null>(
-    null
+    null,
   );
 
   async function fetchDashBoardData() {
@@ -36,28 +21,7 @@ const Dashboard = () => {
   }, []);
 
   return (
-    // <motion.main
-    //   variants={childrenVariant}
-    //   initial="intial"
-    //   animate="animate"
-    //   className="mt-5 mb-2 ml-2"
-    // >
-    //   <motion.div
-    //     variants={childrenVariant}
-    //     className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4"
-    //   >
-    //     <CardWrapper />
-    //   </motion.div>
-    //   <motion.div
-    //     variants={childrenVariant}
-    //     className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8"
-    //   >
-    //     <motion.div variants={childrenVariant}>Latest test results</motion.div>
-
-    //     <motion.div variants={childrenVariant}>Pie chart</motion.div>
-    //   </motion.div>
-    // </motion.main>
-    <main className="grid gap-2 md:grid-cols-2 mt-5 mb-2 ml-2 p-5">
+    <main className="mb-2 ml-2 mt-5 grid gap-2  p-5 md:grid-cols-2">
       <BarChart />
       <div className="">
         {dashboardData?.lastestResults && (
